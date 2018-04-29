201ea30c1b401836ae869dd4efed7ecb36f415f0
David Konopka <dkonopka@uncc.edu>
2018-04-29 22:10:52 +0000

    Added validations for workouts and exercises

--- app/models/excercise.rb
+++ app/models/excercise.rb
@@ -1,3 +1,10 @@
 class Excercise < ApplicationRecord
   belongs_to :workout
+  validates :setrep, presence: true
+  validates :box11, presence: true, numericality: true
+  validates :box12, presence: true, numericality: true
+  validates :box13, presence: true, numericality: true
+  validates :box21, presence: true, numericality: true
+  validates :box22, presence: true, numericality: true
+  validates :box23, presence: true, numericality: true
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,5 @@
 class Workout < ApplicationRecord
     has_many :excercises
+    validates :name, presence: true
+    validates :group, presence: true
 end
