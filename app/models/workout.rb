861cf2861cf314cd3178483ae1c5b7fdd4243dfe
David Konopka <dkonopka@uncc.edu>
2018-04-30 14:19:03 +0000

    fixed destroy bug

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,5 @@
 class Workout < ApplicationRecord
-    has_many :excercises
+    has_many :excercises, dependent: :destroy
     validates :name, presence: true
     validates :group, presence: true
 end
